5aabb4ac35915682d0e9517a015ccd9aeb402885
kk_boomer <fking.dmitry@gmail.com>
2021-07-09 02:30:56 +0200

    Верстка системного монитора

---
+++ lib/until/app_containers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:synt/theme/app_colors.dart';
+
+import 'package:flutter/material.dart';
+
+abstract class AppContainers {
+  static var containerBorder = BoxDecoration(
+    border: Border.all(
+        color: AppColors.accent, width: 1.0, style: BorderStyle.solid),
+  );
+}
---
+++ lib/until/app_paddings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppPaddings {
+  static const defaultPadding = EdgeInsets.all(10);
+  static const dafaultSizedBoxHeight = SizedBox(height: 10);
+  static const dafaultSizedBoxWidth = SizedBox(width: 10);
+}

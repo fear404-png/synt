4349a74320871d7d1023004636618073bfd38a0b
kk_boomer <fking.dmitry@gmail.com>
2021-07-31 04:59:01 +0200

    Верстка сраницы жертвы, добавлен блок для нее

--- lib/pages/components/enemy_item.dart
+++ lib/pages/components/enemy_item.dart
@@ -4,32 +4,47 @@ import 'package:synt/until/app_containers.dart';
 import 'package:synt/until/app_font_style.dart';
 import 'package:synt/until/app_paddings.dart';
 
-class EnemyItem extends StatelessWidget {
+class EnemyItem extends StatefulWidget {
+  final Icon icon;
+  final String name;
   const EnemyItem({
     Key? key,
+    required this.icon,
+    required this.name,
   }) : super(key: key);
 
+  @override
+  State<EnemyItem> createState() => _EnemyItemState();
+}
+
+class _EnemyItemState extends State<EnemyItem> {
+  bool isNotPress = true;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CustomContainer(
-            width: 90,
-            height: 100,
-            child: Column(
-              children: [
-                const Icon(
-                  Icons.dangerous,
-                  color: AppColors.accent,
-                  size: 60,
-                ),
-                Text(
-                  "NAAAAME",
-                  style: AppTextStyle.textStyleHeader,
-                )
-              ],
-            ),
-            crossAxisAlignment: CrossAxisAlignment.center),
+        GestureDetector(
+          onTapDown: (TapDownDetails t) => setState(() => isNotPress = false),
+          onTapUp: (TapUpDetails t) => setState(() => isNotPress = true),
+          onTapCancel: () => setState(() => isNotPress = true),
+          child: CustomContainer(
+              color: isNotPress ? AppColors.background : AppColors.accent,
+              width: 120,
+              height: 120,
+              child: Column(
+                children: [
+                  isNotPress ? widget.icon : SizedBox(),
+                  isNotPress ? AppPaddings.defaultSizedBoxWidth : SizedBox(),
+                  Text(
+                    widget.name,
+                    style: isNotPress
+                        ? AppTextStyle.textStyleHeader
+                        : AppTextStyle.negativeTextStyleTextStyleHeader,
+                  )
+                ],
+              ),
+              crossAxisAlignment: CrossAxisAlignment.center),
+        ),
         AppPaddings.defaultSizedBoxHeight,
       ],
     );
--- lib/pages/enemy_page.dart
+++ lib/pages/enemy_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:synt/blocs/enemy_page_bloc/enemy_page_bloc.dart';
 import 'package:synt/theme/app_colors.dart';
@@ -6,7 +7,7 @@ import 'package:synt/until/app_containers.dart';
 import 'package:synt/until/app_font_style.dart';
 import 'package:synt/until/app_paddings.dart';
 
-import 'components/enemy_items_row.dart';
+import 'components/enemy_item.dart';
 
 class EnemyPage extends StatelessWidget {
   const EnemyPage({Key? key}) : super(key: key);
@@ -20,29 +21,99 @@ class EnemyPage extends StatelessWidget {
           return Scaffold(
             backgroundColor: AppColors.background,
             appBar: AppBar(
+                foregroundColor: AppColors.accent,
                 title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "${state.enemyUser.btc} BTC",
-                  style: AppTextStyle.textStyleHeader,
-                ),
-                Text(
-                  state.enemyUser.ip,
-                  style: AppTextStyle.textStyleHeader,
-                )
-              ],
-            )),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "${state.enemyUser.btc} BTC",
+                      style: AppTextStyle.textStyleHeader,
+                    ),
+                    Text(
+                      state.enemyUser.ip,
+                      style: AppTextStyle.textStyleHeader,
+                    )
+                  ],
+                )),
             body: Padding(
                 padding: AppPaddings.defaultPadding,
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Column(children: [
-                        EnemyItemsRow(),
-                        AppPaddings.defaultSizedBoxHeight
-                      ]);
-                    })),
+                child: SingleChildScrollView(
+                  child: Container(
+                    width: double.infinity,
+                    child: Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      direction: Axis.horizontal,
+                      alignment: WrapAlignment.spaceAround,
+                      children: const [
+                        EnemyItem(
+                          name: "Processes",
+                          icon: Icon(
+                            Icons.replay_outlined,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Scan",
+                          icon: Icon(
+                            Icons.scanner,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Bank Account",
+                          icon: Icon(
+                            Icons.money,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Store",
+                          icon: Icon(
+                            Icons.store,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Messages",
+                          icon: Icon(
+                            Icons.mail,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Log",
+                          icon: Icon(
+                            Icons.text_fields,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Apps",
+                          icon: Icon(
+                            Icons.apps,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                        EnemyItem(
+                          name: "Device",
+                          icon: Icon(
+                            Icons.computer,
+                            color: AppColors.accent,
+                            size: 45,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
           );
         },
       ),
